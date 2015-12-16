20966700c894e74621a48461ee3d8876cc925393
tgb1 <tjeerd@tjeerdbikker.nl>
2015-12-16 15:54:07 +0100

    jQuery CSS manipulation

---
+++ 06-jQuery CSS manipulation/template.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('h1').mouseenter(function() {
+    $(this).toggleClass();
+  });
+});
+
+/*
+
+addClass('classname space classname2')
+removeClass('classname classname2') // Leeglaten om alles te verwijderen
+toggleClass('classname classname2')
+toggleClass()
+
+*/
