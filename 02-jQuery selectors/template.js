b11dc967e8e2bc4a3e4e6e179cfade9e011e4880
tgb1 <tjeerd@tjeerdbikker.nl>
2015-12-16 13:10:39 +0100

    jQuery selectors

---
+++ 02-jQuery selectors/template.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $('#testbutton, strong').click(function() {
+    $(this).css('background-color', 'red');
+  });
+});
+
+/*
+Zelfde als css selectors
+
+desc div strong
+child div > p > strong
+:nth-child(n)
+:first-child, :last-child
+.class
+#id
+p#derde
+
+this (oop) geen quotes
+*/
