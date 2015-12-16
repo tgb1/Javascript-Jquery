3ae0630c4b6cbbe75616ac02ab5c07bd5a5f7b2c
tgb1 <tjeerd@tjeerdbikker.nl>
2015-12-16 13:46:58 +0100

    jQuery events

--- 03-jQuery events/template.js
+++ 03-jQuery events/template.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
+  $('h1').mouseenter(function() {
+    $(this).css('background-color', 'red');
+  });
 
+  $('h1').mouseleave(function() {
+    $(this).css('background-color', 'blue');
+    $('*').unbind('mouseleave');
+  });
 });
+
+/*
+mousedown
+mouseup
+
+mouseenter
+mouseleave
+
+unbind(mouseleave)
+*/
